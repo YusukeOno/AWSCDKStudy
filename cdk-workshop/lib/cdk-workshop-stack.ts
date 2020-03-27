74a26ad1bc0d4286d9b31ecea3e5b659880c8fa2
小野裕輔 <y-ono@nswt.co.jp>
2020-03-27 14:28:13 +0900

    Lambda, DynamoDB, hit counter.

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -4,6 +4,7 @@ import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigw from "@aws-cdk/aws-apigateway";
+import { HitCounter } from "./hitcounter";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -16,9 +17,13 @@ export class CdkWorkshopStack extends cdk.Stack {
       handler: 'hello.handler'                // file is "hello", function is "handler"
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
     // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     });
   }
 }
