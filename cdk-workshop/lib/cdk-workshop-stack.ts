457050facb2e5ad1c630d0aeeb269c763f720639
小野裕輔 <y-ono@nswt.co.jp>
2020-03-27 17:12:25 +0900

    Add table viewer to the stack.

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -5,6 +5,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigw from "@aws-cdk/aws-apigateway";
 import { HitCounter } from "./hitcounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -24,6 +25,11 @@ export class CdkWorkshopStack extends cdk.Stack {
     // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
     });
   }
 }
