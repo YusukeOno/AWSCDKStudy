6963311c1d78b9a7c527432cf2ef654c0f8280f4
小野裕輔 <y-ono@nswt.co.jp>
2021-08-08 23:54:35 +0900

    [wip] Add pipeline

---
+++ cdk-workshop/lib/pipeline-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class WorkshopPipelineStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // Pipeline code goes here
+    }
+}
